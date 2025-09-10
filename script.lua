8ef51d892dd83086533218f7c3d37e19d99ab38e
GAGScriptV3 <osaragihub@gmail.com>
2025-09-10 22:40:49 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/egg/refs/heads/main/lua"))()
 loadstring(game:HttpGet("https://pastefy.app/B00Jc84x/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GAGScriptV3/egg/refs/heads/main/lua"))()
